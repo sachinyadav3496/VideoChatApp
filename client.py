ee637549090fdd8f9c00211759b84b17c986615b
sachinyadav3496 <sachinyadav3496@gmail.com>
2020-10-07 02:46:09 +0530

    second version a glitchy version will fix soon

--- client.py
+++ client.py
@@ -2,6 +2,8 @@ import socket
 import os
 import cv2
 
+cap = cv2.VideoCapture(1)
+
 client = socket.socket()
 
 client.connect(('localhost', 12345))
@@ -11,7 +13,7 @@ print("connected")
 
 
 while True:
-    s = client.recv(20)
+    s = client.recv(6)
     print(s)
     size = int(s.decode())
     print("size: ", size)
@@ -28,6 +30,16 @@ while True:
     im = cv2.imread('test_recv.jpg')
     cv2.imshow('Image', im)
     cv2.waitKey(1)
+
+    _, im = cap.read()
+    cv2.imwrite('test1.jpg', im)
+    fp = open('test1.jpg', 'rb')
+    client.send(f"{os.path.getsize('test1.jpg')}".encode())
+    print("Sending File")
+    client.sendfile(fp)
+    print("file Send")
+    fp.close()
+    cv2.waitKey(20)
 
 print("sucessfull")
 
--- server.py
+++ server.py
@@ -26,8 +26,26 @@ while True:
 
     fp.close()
     
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
+    cv2.waitKey(100)
+    s = client.recv(5)
+    print(s)
+    size = int(s.decode())
+    print("Size: ", size)
+    recv_size = 0
+    file_data = open('test_recv1.jpg', 'wb')
+    print("opend file")
+    while recv_size < size:
+        data = client.recv(size)
+        file_data.write(data)
+        recv_size += len(data)
+        print('received data: ', recv_size)
+    file_data.close()
+    print("Recevied file")
+    im = cv2.imread('test_recv1.jpg')
+    cv2.imshow('Image1', im)
+    cv2.waitKey(10)
+
+
 
 client.close()
 
